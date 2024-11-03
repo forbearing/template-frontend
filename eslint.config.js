0eebd4d0f591c044eef2e5ce5fb80faa86ab9995
hybfkuf <fredjoseph863@gmail.com>
2024-11-03 18:04:33 +0800

    chg(eslint) ignore none-name compoents export

--- eslint.config.js
+++ eslint.config.js
@@ -24,6 +24,7 @@ export default tseslint.config(
       '@typescript-eslint/no-unused-vars': 'warn',
       '@typescript-eslint/no-explicit-any': 'warn',
       '@typescript-eslint/no-unsafe-function-type': 'warn',
+      'react-refresh/only-export-components': 'off',
     },
   },
 )
